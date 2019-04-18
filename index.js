1a79e6f4eb7e89dcf7aa9583318d9cdc8fddc021
Bart van Raaij <bart@linku.nl>
2019-04-18 17:21:13 +0200

    First tool commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const phpParser = require('php-parser'),
+      fs = require('fs');
+
+const readPhpArray = {
+
+  fromFile: function(file) {
+    const phpString = fs.readFileSync(file, 'utf8');
+    return readPhpArray.fromString(phpString);
+  },
+
+  fromString: function(phpString){
+
+    const parser = new phpParser({
+      parser: {
+        extractDoc: false,
+        suppressErrors: true
+      },
+      ast: { withPositions: false },
+    });
+
+    if(phpString.substr(0,4)!=='<?php') {
+      phpString = '<?php \n'+phpString;
+    }
+
+    const ast = parser.parseCode(phpString);
+
+    let phpObject = {};
+    if (ast.kind === 'program') {
+
+      ast.children.forEach(child => {
+
+        if(child.kind==='expressionstatement' && child.expression.operator === '=' && child.expression.left.kind === 'variable' && child.expression.right.kind === 'array') {
+          phpObject[child.expression.left.name] = readPhpArray.parseValue(child.expression.right);
+        }
+        else if(child.kind==='expressionstatement' && child.expression.kind === 'array'){
+          phpObject = readPhpArray.parseValue(child.expression);
+        }
+        else if(child.kind === 'return' && child.expr.kind === 'array') {
+          phpObject = readPhpArray.parseValue(child.expr);
+        }
+
+      });
+
+    }
+    return phpObject;
+  },
+
+  /**
+   * Parse a PHP expression to JavaScript
+   * @param  {Object} expr The AST PHP expression.
+   * @return {*}           A JavaScript object or value.
+   */
+  parseValue: function(expr) {
+    if(expr===null) return null;
+    switch(expr.kind) {
+      case 'array':
+        if (expr.items.length === 0) {
+          return [];
+        }
+        const isKeyed = expr.items.every((item) => item !== null && typeof item.key!=='undefined' && item.key !== null);
+        let items = expr.items.map(readPhpArray.parseValue);
+        if (isKeyed) {
+          items = items.reduce((acc, val) => Object.assign({}, acc, val), {})
+        }
+        return items;
+      case 'entry':
+        if (expr.key) {
+          return {
+            [readPhpArray.parseKey(expr.key)]: readPhpArray.parseValue(expr.value)
+          }
+        }
+        return readPhpArray.parseValue(expr.value);
+      case 'string':
+        return expr.value;
+      case 'number':
+        return parseInt(expr.value, 10);
+      case 'boolean':
+        return expr.value;
+      default:
+        return null;
+    }
+  },
+
+  /**
+   * Parse a PHP expression to JavaScript
+   * @param  {Object} expr The AST PHP expression.
+   * @return {*}           A JavaScript object or value.
+   */
+  parseKey: function(expr) {
+    switch(expr.kind) {
+      case 'string':
+        return expr.value;
+      case 'number':
+        return parseInt(expr.value, 10);
+      case 'boolean':
+        return expr.value ? 1 : 0;
+      default:
+        return null;
+    }
+  }
+
+};
+
+module.exports = readPhpArray;
