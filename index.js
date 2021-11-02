2dddd5f180b9eb82991f91e7ab2cd1f5450eaf00
Bart van Raaij <bart@linku.nl>
2021-11-02 17:14:38 +0100

    Improve code quality

--- index.js
+++ index.js
@@ -1,113 +1,112 @@
 const phpParser = require('php-parser'),
       fs = require('fs');
 
-const readPhpArray = {
 
-  fromFile: function(file) {
-    const phpString = fs.readFileSync(file, 'utf8');
-    return readPhpArray.fromString(phpString);
-  },
 
-  fromString: function(phpString){
-
-    const parser = new phpParser({
-      parser: {
-        extractDoc: false,
-        suppressErrors: true
-      },
-      ast: { withPositions: false },
-    });
-
-    phpString = phpString.trim();
-    if(phpString.substr(0,5)!=='<?php') {
-      phpString = '<?php \n'+phpString;
-    }
-
-    const ast = parser.parseCode(phpString);
-
-    let phpObject = {};
-    if (ast.kind === 'program') {
-
-      ast.children.forEach(child => {
-
-        if(child.kind==='expressionstatement' && child.expression.operator === '=' && child.expression.left.kind === 'variable' && child.expression.right.kind === 'array') {
-          phpObject[child.expression.left.name] = readPhpArray.parseValue(child.expression.right);
+/**
+ * Parse a PHP expression to JavaScript
+ * @param  {Object} expr The AST PHP expression.
+ * @return {*}           A JavaScript object or value.
+ */
+const parseValue = function(expr) {
+  if(expr===null) return;
+  switch(expr.kind) {
+    case 'array':
+      if (expr.items.length === 0) {
+        return [];
+      }
+      const isKeyed = expr.items.every(item =>
+        item === null || item.value === undefined || (item.key!==undefined && item.key !== null)
+      );
+      let items = expr.items.map(parseValue).filter(itm => itm !== undefined);
+      if (isKeyed) {
+        items = items.reduce((acc, val) => Object.assign({}, acc, val), {})
+      }
+      return items;
+    case 'entry':
+      if (expr.key) {
+        return {
+          [parseKey(expr.key)]: parseValue(expr.value)
         }
-        else if(child.kind==='expressionstatement' && child.expression.kind === 'array'){
-          phpObject = readPhpArray.parseValue(child.expression);
-        }
-        else if(child.kind === 'return' && child.expr.kind === 'array') {
-          phpObject = readPhpArray.parseValue(child.expr);
-        }
-
-      });
-
-    }
-    return phpObject;
-  },
-
-  /**
-   * Parse a PHP expression to JavaScript
-   * @param  {Object} expr The AST PHP expression.
-   * @return {*}           A JavaScript object or value.
-   */
-  parseValue: function(expr) {
-    if(expr===null) return;
-    switch(expr.kind) {
-      case 'array':
-        if (expr.items.length === 0) {
-          return [];
-        }
-        const isKeyed = expr.items.every(item =>
-          item === null || item.value === undefined || (item.key!==undefined && item.key !== null)
-        );
-        let items = expr.items.map(readPhpArray.parseValue).filter(itm => itm !== undefined);
-        if (isKeyed) {
-          items = items.reduce((acc, val) => Object.assign({}, acc, val), {})
-        }
-        return items;
-      case 'entry':
-        if (expr.key) {
-          return {
-            [readPhpArray.parseKey(expr.key)]: readPhpArray.parseValue(expr.value)
-          }
-        }
-        return readPhpArray.parseValue(expr.value);
-      case 'string':
-        return expr.value;
-      case 'number':
-        return parseFloat(expr.value);
-      case 'boolean':
-        return expr.value;
-      case 'nullkeyword':
+      }
+      return parseValue(expr.value);
+    case 'string':
+      return expr.value;
+    case 'number':
+      return parseFloat(expr.value);
+    case 'boolean':
+      return expr.value;
+    case 'nullkeyword':
+      return null;
+      break;
+    case 'identifier':
+      if(expr.name.name==='null') {
         return null;
-        break;
-      case 'identifier':
-        if(expr.name.name==='null') {
-          return null;
-        }
-        break;
-    }
-  },
-
-  /**
-   * Parse a PHP expression to JavaScript
-   * @param  {Object} expr The AST PHP expression.
-   * @return {*}           A JavaScript object or value.
-   */
-  parseKey: function(expr) {
-    switch(expr.kind) {
-      case 'string':
-        return expr.value;
-      case 'number':
-        return parseFloat(expr.value);
-      case 'boolean':
-        return expr.value ? 1 : 0;
-      default:
-        return null;
-    }
+      }
+      break;
   }
-
 };
 
-module.exports = readPhpArray;
+/**
+ * Parse a PHP expression to JavaScript
+ * @param  {Object} expr The AST PHP expression.
+ * @return {*}           A JavaScript object or value.
+ */
+const parseKey = function(expr) {
+  switch(expr.kind) {
+    case 'string':
+      return expr.value;
+    case 'number':
+      return parseFloat(expr.value);
+    case 'boolean':
+      return expr.value ? 1 : 0;
+    default:
+      return null;
+  }
+};
+
+
+const fromString = function(phpString) {
+
+  const parser = new phpParser({
+    parser: {
+      extractDoc: false,
+      suppressErrors: true
+    },
+    ast: { withPositions: false },
+  });
+
+  phpString = phpString.trim();
+  if(phpString.substr(0,5)!=='<?php') {
+    phpString = '<?php \n'+phpString;
+  }
+
+  const ast = parser.parseCode(phpString);
+
+  let phpObject = {};
+  if (ast.kind === 'program') {
+
+    ast.children.forEach(child => {
+
+      if(child.kind==='expressionstatement' && child.expression.operator === '=' && child.expression.left.kind === 'variable' && child.expression.right.kind === 'array') {
+        phpObject[child.expression.left.name] = parseValue(child.expression.right);
+      }
+      else if(child.kind==='expressionstatement' && child.expression.kind === 'array'){
+        phpObject = parseValue(child.expression);
+      }
+      else if(child.kind === 'return' && child.expr.kind === 'array') {
+        phpObject = parseValue(child.expr);
+      }
+
+    });
+
+  }
+  return phpObject;
+};
+
+const fromFile = function(file) {
+  const phpString = fs.readFileSync(file, 'utf8');
+  return fromString(phpString);
+}
+
+module.exports = { fromFile, fromString };
