b84b0c20c38ceb8d49c7a9ae98db7d241311b598
Bart van Raaij <info@bartvanraaij.nl>
2023-08-17 10:11:44 +0200

    v2.0.0: Remove file support, remove node 14 support, add TS type declaration (#8)
    * Remove file support, node 14 support, add node 20 support
    
    * Update readme
    
    * Add ts type declaration, code style fixes
    
    * Improve readme, improve test
    
    * Improved code style, add boolean test

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 [![Node.js CI](https://github.com/bartvanraaij/php-array-reader/actions/workflows/node.js.yml/badge.svg)](https://github.com/bartvanraaij/php-array-reader/actions/workflows/node.js.yml)
 
-This small JS utility reads PHP files and strings containing arrays and returns a JavaScript object. 
+This small JS utility reads PHP strings containing arrays and returns a JavaScript object. 
 
 It uses [glayzzle/php-parser](https://github.com/glayzzle/php-parser) to parse PHP into AST and uses that 
 info to extract arrays.  
-It supports both indexed and associative arrays and array, string, numeric and null values.
+It supports both indexed and associative arrays (i.e. lists and dictionaries/maps) and array, string, numeric and null values.
 
 ## Installation
 
@@ -18,57 +18,70 @@ npm install php-array-reader --save
 
 ## Usage
 
-### With a PHP string
 ```js
-const phpArrayReader = require('php-array-reader');
+import { fromString } from 'php-array-reader';
 
 const phpString = `[
   'key' => 'string',
-  'indexed_array' => [
+  'list' => [
     'first',
     'second'
   ],
-  'associative_array' => [
+  'dictionary' => [
     'foo' => 'bar',
     'hello' => 'world'
   ],
   'also_supports' => null,
-  'and_numeric' => 42
+  'and_numeric' => 42,
+  'what_about' => true,
+  'or' => false,
 ]`;
-const data = phpArrayReader.fromString(phpString); 
+const data = fromString(phpString); 
 ```
 `data` will be this JS object:
 ```js
 {
   key: 'string',
-  indexed_array: ['first', 'second'],
-  associative_array: {
+  list: ['first', 'second'],
+  dictionary: {
     foo: 'bar',
     hello: 'world'
   },
   also_supports: null,
-  and_numeric: 42
+  and_numeric: 42,
+  what_about: true,
+  or: false
 }
 ```
 
 ### With a PHP file
+
+Use [`fs.readFileSync`](https://nodejs.org/api/fs.html#fsreadfilesyncpath-options) or another file reading library to read the file, and pass 
+that string into `fromString`, e.g.:
 ```js
-const phpArrayReader = require('php-array-reader');
+import { fromString } from 'php-array-reader';
+import { readFileSync } from 'node:fs';
 
 const phpFile = './file.php';
-const data = phpArrayReader.fromFile(phpFile);
+const phpString = readFileSync(phpFile);
+
+const data = fromString(phpFile);
 ```
+
+> [!NOTE]
+> Version `1.x` of this library included a [`fromFile` method](https://github.com/bartvanraaij/php-array-reader/blob/a3f48acdef4eace2106ac40fa3c4593ab196dc1c/index.js#L6) 
+> that allowed you to read a file directly. This has been removed in version `2.x` forward, because that method was a scope creep.
 
 The PHP file can either return a single array, e.g.:
 ```php
 <?php
 return [
    'key' => 'string',
-   'indexed_array' => [
+   'list' => [
      'first',
      'second'
    ],
-   'associative_array' => [
+   'dictionary' => [
      'foo' => 'bar',
      'hello' => 'world'
    ],
@@ -84,13 +97,13 @@ Or the PHP file may consist of multiple assigned arrays, e.g.:
 <?php
 $first = [
     'key' => 'string',
-    'associative_array' => [
+    'dictionary' => [
         'foo' => 'bar',
         'hello' => 'world'
     ]
 ];
 $second = [
-    'index_array' => [
+    'list' => [
         'first','second'
     ],
     'also_supports' => null,
@@ -103,23 +116,15 @@ This will return a JS object with the variable names as the first level keys:
 {
   first: {
     key: 'string',
-    associative_array: {
+    dictionary: {
       foo: 'bar', 
       hello: 'world'
     }
   },
   second: {
-    index_array: ['first', 'second'],
+    list: ['first', 'second'],
     also_supports: null,
     and_numeric: 42
   }
 }
 ```
-You can then of course also use destructuring to assign the results to two variables:
-```js
-const phpArrayReader = require('php-array-reader');
-
-const phpFile = './file.php';
-const { first, second } = phpArrayReader.fromFile(phpFile);
-```
-
--- index.test.js
+++ index.test.js
@@ -1,162 +1,81 @@
+const { fromString } = require('./index');
 
-const mockFs = require('mock-fs');
-const phpArrayReader = require('./index');
-
-const indexStr = `[
+test('list', () => {
+  const data = fromString(`[
     'foo', 'bar', 'baz'
-]`;
-const associativeStr = `[
-  'foo' => 'bar',
-  'hello' => 'world'
-]`;
-const valueTypesStr = `[
-  'arrayvalue' => [
-    'hello', 'world'
-  ],
-  'stringvalue' => 'foo',
-  'numbervalue' => 42,
-  'decimalvalue' => 4.2,
-  'nullvalue' => null
-]`;
-const multidimensionalStr = `[
-  'first' => [
-    'second' => [
-      'third' => 1
+  ]`);
+  expect(data).toEqual([
+    'foo', 'bar', 'baz'
+  ]);
+});
+
+test('dictionary', () => {
+  const data = fromString(`[
+    'foo' => 'bar',
+    'hello' => 'world'
+  ]`);
+  expect(data).toEqual({
+    foo: 'bar',
+    hello: 'world'
+  });
+});
+
+test('multidimensional', () => {
+  const data = fromString(`[
+    'first' => [
+      'second' => [
+        'third' => 1
+      ],
+      'fourth' => 4
+    ]
+  ]`);
+  expect(data).toEqual({
+    first: {
+      second: {
+        third: 1
+      },
+      fourth: 4
+    }
+  });
+});
+
+test('multiple', () => {
+  const data = fromString(`
+    $first = ['foo', 'bar'];
+    $second = ['hello', 'world'];
+  `);
+  expect(data).toEqual({
+    first: ['foo', 'bar'],
+    second: ['hello', 'world']
+  });
+});
+
+test('php file', () => {
+  const data = fromString(`<?php
+    return ['foo', 'bar'];
+  `);
+  expect(data).toEqual(['foo', 'bar']);
+});
+
+test('all value types match (arrays strings numbers decimals nulls booleans)', () => {
+  const data = fromString(`[
+    'arrayvalue' => [
+      'hello', 'world'
     ],
-    'fourth' => 4
-  ]
-]`;
-
-const multipleStr = `
-  $first = ['foo', 'bar'];
-  $second = ['hello', 'world'];
-`;
-
-mockFs({
-  'indexed.php': `<?php \n return ${indexStr}`,
-  'associative.php': `<?php \n return ${associativeStr}`,
-  'value-types.php': `<?php \n return ${valueTypesStr}`,
-  'multidimensional.php': `<?php \n return ${multidimensionalStr}`,
-  'multiple.php': `<?php \n  ${multipleStr}`
-});
-
-
-
-afterAll(() => {
-  mockFs.restore();
-
-});
-
-// jest.mock('fs');
-
-test('indexed array from string', () => {
-
-  const data = phpArrayReader.fromString(indexStr);
-  expect(data).toEqual([
-    'foo', 'bar', 'baz'
-  ])
-
-});
-
-test('indexed array from file', () => {
-
-  const data = phpArrayReader.fromFile('indexed.php');
-  expect(data).toEqual([
-    'foo', 'bar', 'baz'
-  ])
-
-});
-
-
-test('associative array from string', () => {
-
-  const data = phpArrayReader.fromString(associativeStr);
-
+    'stringvalue' => 'foo',
+    'numbervalue' => 42,
+    'decimalvalue' => 4.2,
+    'nullvalue' => null,
+    'truevalue' => true,
+    'falsevalue' => false,
+  ]`);
   expect(data).toEqual({
-    foo: 'bar',
-    hello: 'world'
-  });
-
-});
-
-
-test('associative array from file', () => {
-
-  const data = phpArrayReader.fromFile('associative.php');
-
-  expect(data).toEqual({
-    foo: 'bar',
-    hello: 'world'
-  });
-
-});
-
-test('all value types match (arrays strings numbers decimals nulls) from string', () => {
-
-  const data = phpArrayReader.fromString(valueTypesStr);
-
-  expect(data).toEqual({
-    arrayvalue: ['hello','world'],
+    arrayvalue: ['hello', 'world'],
     stringvalue: 'foo',
     numbervalue: 42,
     decimalvalue: 4.2,
-    nullvalue: null
+    nullvalue: null,
+    truevalue: true,
+    falsevalue: false
   });
-
-});
-
-test('all value types match (arrays strings numbers decimals nulls) from file', () => {
-
-  const data = phpArrayReader.fromFile('value-types.php');
-
-  expect(data).toEqual({
-    arrayvalue: ['hello','world'],
-    stringvalue: 'foo',
-    numbervalue: 42,
-    decimalvalue: 4.2,
-    nullvalue: null
-  });
-
-});
-
-test('multidimensional array from string', () => {
-
-  const data = phpArrayReader.fromString(multidimensionalStr);
-
-  expect(data).toEqual({
-    first: {
-      second: {
-        third: 1
-      },
-      fourth: 4
-    }
-  });
-
-});
-
-test('multidimensional array from file', () => {
-
-  const data = phpArrayReader.fromFile('multidimensional.php');
-
-  expect(data).toEqual({
-    first: {
-      second: {
-        third: 1
-      },
-      fourth: 4
-    }
-  });
-
-});
-
-
-test('multiple arrays from file', () => {
-
-  const data = phpArrayReader.fromFile('multiple.php');
-
-  expect(data).toEqual({
-    first: ['foo','bar'],
-    second: ['hello' , 'world']
-  });
-
 });
